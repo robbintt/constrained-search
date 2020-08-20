322d93459de4ae65442960a3fa49f82664a13c93
Trent Robbins <robbintt@gmail.com>
2020-08-19 19:58:09 -0700

    Get js redirect working with button.

---
+++ constructsearch.js
@@ -0,0 +1,19 @@
+/*
+ * Goal:
+ * - Add a list of acceptable sites to the contents of a textbox for search
+ *
+ * References:
+ * ultimate google search params: https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters
+ *
+ *
+ * A url:
+ * https://www.google.com/search?source=hp&ei=Tt89X-OJNtXB7gKxiLvIDA
+ * &q=site%3Areddit.com+OR+site%3Anews.ycombinator.com+potato
+ *
+ *
+ * https://www.google.com/search?ei=Wd89X8HNOMPv9APdlL-YCQ&q=site%3Areddit.com+OR+site%3Anews.ycombinator.com+potato&oq=site%3Areddit.com+OR+site%3Anews.ycombinator.com+potato&gs_lcp=CgZwc3ktYWIQA1D71gxYnN0MYKbeDGgCcAB4AIABTYgBowSSAQE5mAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwiB3sfK3ajrAhXDN30KHV3KD5MQ4dUDCAw&uact=5
+*/
+
+$("button").click(function() {
+  window.location.assign("http://google.com?s=potato");
+});
