274c1e70a0586333b48e6c694c644348deff7f0a
Trent Robbins <robbintt@gmail.com>
2020-08-19 23:47:01 -0700

    Fix s3 bucket in push_site

--- push_site.sh
+++ push_site.sh
@@ -6,5 +6,5 @@ favicon.ico
 "
 for file in $files
 do
-  aws s3 cp $file s3://adv-search-static-site-d5f00b24-956f-4d31-8eb0-44e5d7e70834
+  aws s3 cp $file s3://search-static-site-d5f00b24-956f-4d31-8eb0-44e5d7e70834
 done
