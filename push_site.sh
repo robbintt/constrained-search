8b33b24d4e15a63ca84e99106ffc631dbc89cd79
Trent Robbins <robbintt@gmail.com>
2020-08-19 23:30:36 -0700

    Update favicon, update project name, generalize push_site.sh

--- push_site.sh
+++ push_site.sh
@@ -1,3 +1,10 @@
-aws s3 cp allowed_sites.txt s3://adv-search-static-site-d5f00b24-956f-4d31-8eb0-44e5d7e70834
-aws s3 cp construct_search.js s3://adv-search-static-site-d5f00b24-956f-4d31-8eb0-44e5d7e70834
-aws s3 cp index.html s3://adv-search-static-site-d5f00b24-956f-4d31-8eb0-44e5d7e70834
+export files="
+allowed_sites.txt
+construct_search.js
+index.html
+favicon.ico
+"
+for file in $files
+do
+  aws s3 cp $file s3://adv-search-static-site-d5f00b24-956f-4d31-8eb0-44e5d7e70834
+done
