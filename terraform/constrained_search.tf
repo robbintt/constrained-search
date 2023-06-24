c94cc0585e1718fa05d8d399f0e803a49403190a
Trent Robbins <robbintt@gmail.com>
2023-06-23 22:24:04 -0700

    Remove old domain bucket.

--- terraform/constrained_search.tf
+++ terraform/constrained_search.tf
@@ -14,18 +14,6 @@ resource "aws_route53_record" "search" {
   }
 }
 
-resource "aws_s3_bucket" "search_site_bucket" {
-  bucket = "search.tauinformatics.com"
-  tags   = merge(local.base_tags, {})
-  acl    = "public-read"
-  policy = data.aws_iam_policy_document.search_s3_public_read.json
-
-  website {
-    index_document = "index.html"
-  }
-}
-
-# note there is a new aws_s3_bucket_website_configuration resource... maybe try it
 resource "aws_s3_bucket" "search_ftl_cc_bucket" {
   bucket = "search.ftl.cc"
   tags   = merge(local.base_tags, {})
@@ -74,22 +62,6 @@ resource "aws_s3_bucket_website_configuration" "search_ftl_cc" {
 # nb: comment this and comment the s3 bucket policy to get the bucket ARN, they are cyclic
 # policy from: https://docs.aws.amazon.com/AmazonS3/latest/user-guide/static-website-hosting.html
 # wildcard principal: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document#wildcard-principal
-data "aws_iam_policy_document" "search_s3_public_read" {
-  statement {
-    sid = "PublicReadGetObject"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:getobject"
-    ]
-    resources = [
-      "arn:aws:s3:::search.tauinformatics.com/*"
-    ]
-  }
-}
-
 data "aws_iam_policy_document" "search_ftl_cc_s3_public_read" {
   statement {
     sid = "PublicReadGetObject"
